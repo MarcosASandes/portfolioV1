d2915e83ba6070537028eb9cf2d95303c65f0e04
MarcosASandes <agustin0818sandes@gmail.com>
2024-04-19 16:29:08 -0300

    Agregado de tecnologías y texto en conoceme

--- src/Components/SobreMi.jsx
+++ src/Components/SobreMi.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { guardarTextos } from '../features/sobreMiSlice';
 import { useSelector } from 'react-redux';
+import Tecnologias from './Tecnologias';
 
 
 const SobreMi = () => {
@@ -21,7 +22,47 @@ const SobreMi = () => {
     
 
     useEffect(() => {
-        const textos = ['Texto del Pasado', 'Texto del Presente', 'Texto del Futuro'];
+
+        let pasado = `Desde mi infancia, la tecnología ha sido una pasión constante en mi vida. 
+        Mis primeros encuentros con el mundo digital se dieron a través del diseño gráfico y la edición de vídeos, 
+        lo que despertó en mí una profunda fascinación por el proceso de convertir ideas en realidades tangibles. 
+        Este interés me llevó a explorar el arte digital y electrónico durante un año en mi formación académica en Bellas Artes.
+        
+        Durante ese tiempo, tuve mis primeros acercamientos a la programación con Javascript, lo que despertó mi curiosidad
+        y me llevó a adentrarme más en el campo de la tecnología. 
+        Fue entonces cuando decidí seguir una carrera en Analista en Tecnologías de la Información en la Universidad ORT.
+        
+        A lo largo de mi trayectoria académica, he cultivado diversas habilidades técnicas y blandas, 
+        siempre enfocado en mantener una ética de trabajo rigurosa y organizada. 
+        Estoy comprometido con la excelencia en todo lo que hago y estoy emocionado por seguir creciendo y 
+        aprendiendo en este apasionante mundo tecnológico.`;
+
+
+        let presente = `Actualmente, me encuentro en la recta final de mi último semestre universitario, 
+        a punto de enfrentarme al proyecto final que marcará el cierre de esta etapa académica. 
+        Mientras tanto, estoy inmerso en el desarrollo de varios proyectos personales, con el objetivo de ampliar mis 
+        conocimientos y reforzar las habilidades que he adquirido hasta el momento.
+        
+        Además de mi enfoque en el ámbito técnico, estoy dedicando tiempo al estudio de metodologías ágiles. 
+        Aunque ya he completado un curso en este campo, mi objetivo actual es profundizar mis conocimientos 
+        y obtener una comprensión más sólida. Para mí, la ejecución de una idea es tan crucial como la idea misma, y estoy comprometido 
+        a aprender y crecer continuamente en este proceso.`;
+
+
+        let futuro = `Si bien el futuro es incierto, mantengo el deseo de un futuro en el que tanto mi desarrollo 
+        profesional como personal alcanzarán nuevas cotas de excelencia. En el ámbito técnico, aspiro a forjar 
+        una ética de trabajo sólida y distintiva, que refleje mi visión del mundo y con la cual me sienta profundamente orgulloso.
+        
+        En lo que respecta al backend, estoy decidido a sumergirme aún más en el vasto ecosistema de Microsoft, 
+        explorando cada faceta para dominarla por completo. Por otro lado, en el ámbito del frontend, 
+        me apasiona profundamente el uso de React y estoy comprometido a perfeccionar mis habilidades en esta tecnología líder. 
+        Además, como objetivo personal, albergo un gran interés en adentrarme en el fascinante mundo de ThreeJS 
+        y otras tecnologías afines, buscando expandir mis horizontes y explorar nuevas posibilidades creativas.`;
+
+
+
+
+        const textos = [pasado, presente, futuro];
         dispatch(guardarTextos(textos));
     }, []);
 
@@ -62,9 +103,9 @@ const SobreMi = () => {
                             <div className={`circulo ${subtitulo === "Futuro" ? "active" : ""}`} onClick={() => handleCambioTexto('Futuro')}></div>
                         </div>
                     </div>
-                    <div className="col-6">
+                    <div className="col-6 text-right">
                         <h3>Tecnologías</h3>
-                        {/* Aquí irá el contenido de Tecnologías */}
+                        <Tecnologias />
                     </div>
                 </div>
             </div>
--- src/Components/Tecnologias.jsx
+++ src/Components/Tecnologias.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import jsImage from '../img_tecnologias/js.jpeg';
-import csharpImage from '../img_tecnologias/csharp.jpeg';
-import reactImage from '../img_tecnologias/react.jpeg';
+import jsImage from '../imgs_logos/js.png';
+import csharpImage from '../imgs_logos/c-sharp.png';
+import reactImage from '../imgs_logos/physics.png';
+import bootsImage from '../imgs_logos/bootstrap.png';
+import cssImage from '../imgs_logos/css-3.png';
+import githubImage from '../imgs_logos/github.png';
+import htmlImage from '../imgs_logos/html-5.png';
+import javaImage from '../imgs_logos/java.png';
+import gitImage from '../imgs_logos/social.png';
+import sqlImage from '../imgs_logos/icons8-servidor-microsoft-sql-480.png';
+import reduxImage from '../imgs_logos/icons8-redux-480.png';
+import netImage from '../imgs_logos/icons8-net-framework-480.png';
+
 
 
 const Tecnologias = () => {
@@ -10,6 +20,15 @@ const Tecnologias = () => {
             <img src={jsImage} alt="JavaScript" className="tecnologia" />
             <img src={csharpImage} alt="C#" className="tecnologia" />
             <img src={reactImage} alt="React" className="tecnologia" />
+            <img src={bootsImage} alt="Bootstrap" className="tecnologia" />
+            <img src={cssImage} alt="CSS" className="tecnologia" />
+            <img src={githubImage} alt="Github" className="tecnologia" />
+            <img src={htmlImage} alt="HTML" className="tecnologia" />
+            <img src={javaImage} alt="Java" className="tecnologia" />
+            <img src={gitImage} alt="Git" className="tecnologia" />
+            <img src={sqlImage} alt="SQL Server" className="tecnologia" />
+            <img src={reduxImage} alt="Redux" className="tecnologia" />
+            <img src={netImage} alt=".Net Framework" className="tecnologia" />
 
 
             {/* Agrega más imágenes de tecnologías según necesites */}
